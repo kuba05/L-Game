9a72bbeed2bdbba09a246fe2f2969e9735d37765
Maneren <49210777+Maneren@users.noreply.github.com>
2021-06-08 23:41:19 +0200

    applied the ts-standard code style

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import react from 'react';
-import './App.css';
-import { Piece } from './piece';
-import { Map } from './map';
+import react from 'react'
+import './App.css'
+import { Piece } from './Piece'
+import { Map } from './Map'
 
-class App extends react.Component{
-    render() {
-    
-        let players = ["Player1", "Player2"];
-        
-        let p = [
-            new Piece('blue', [2,2], [1,1,1], [[0,0],[0,1],[0,2],[1,0]]),
-            new Piece('green', [5,5], [-1,-1,1], [[0,0],[0,1],[0,2],[1,0]]),
-            new Piece('black', [4,3], [1,1,1], [[0,0]]),
-            new Piece('black', [3,4], [1,1,1], [[0,0]])
-        ];
-        
-        let moveOrder = [
-            [p[0]],
-            [p[2],p[3]],
-            [p[1]],
-            [p[2],p[3]]
-        ];
-        
-        let playersForMoves = [0,0,1,1];
-        
-        let mandatoryMoves = [0,2];
-        
-        return <><Map pieces={p} moveOrder={moveOrder} mandatoryMoves={mandatoryMoves} players={players} playersForMoves={playersForMoves}/></>;
-    }
+class App extends react.Component {
+  render (): React.ReactNode {
+    const players = ['Player 1', 'Player 2']
+
+    const p = [
+      new Piece('blue', [2, 2], [1, 1, 1], [[0, 0], [0, 1], [0, 2], [1, 0]]),
+      new Piece('green', [5, 5], [-1, -1, 1], [[0, 0], [0, 1], [0, 2], [1, 0]]),
+      new Piece('black', [4, 3], [1, 1, 1], [[0, 0]]),
+      new Piece('black', [3, 4], [1, 1, 1], [[0, 0]])
+    ]
+
+    const [blue, green, black1, black2] = p
+
+    const moveOrder = [
+      [blue],
+      [black1, black2],
+      [green],
+      [black1, black2]
+    ]
+
+    const playersForMoves = [0, 0, 1, 1]
+
+    const mandatoryMoves = [0, 2]
+
+    return (
+      <Map
+        pieces={p}
+        moveOrder={moveOrder}
+        mandatoryMoves={mandatoryMoves}
+        players={players}
+        playersForMoves={playersForMoves}
+      />
+    )
+  }
 }
 
-export default App;
+export default App
